bf785d6e0bf70dd4be147b8eafd73ad5b278867e
Gabriel Lisboa <spgabriel08@hotmail.com>
2022-04-15 13:56:20 -0300

    Adicionado try cat e informado como comentario como puxar a classe para utilizar o template

--- MyStack.cs
+++ MyStack.cs
@@ -1,12 +1,11 @@
-using System;
-using Configs;
-using Resources;
-using System.Text.Json;
 using Pulumi;
 
 class MyStack : Stack
 {
     public MyStack()
     {
+        // To use as a template, place the call below and fill in Pulumi.dev
+        // var svc = new Template.IaC.WebApp();
+        // svc.Create();
     }
 }
